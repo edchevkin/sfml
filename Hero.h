089775b5405a84d6bc7f5efeefa9ba0f5fbbf2d8
echevkin <echevkin@gmail.com>
2021-06-07 01:52:04 +0300

    commit #2 07.06 1:49

--- Hero.h
+++ Hero.h
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include "map.h"
 
 using namespace sf;
 using namespace std;
@@ -7,18 +8,16 @@ class Hero {
 public:
     float x, y, w, h, dx, dy, speed = 0;
     int direction = 0;
-    String File;
     Image image;
     Texture texture;
     Sprite sprite;
     View view;
-    Hero(String F, float X, float Y, float W, float H) {
-        File = F;
+    Hero(float X, float Y, float W, float H) {
         w = W; h = H;
-        image.loadFromFile("images/" + File);
+        image.loadFromFile("images/hero.png");
         texture.loadFromImage(image);
         sprite.setTexture(texture);
-        sprite.setTextureRect(IntRect(0, 0, 32, 48));
+        sprite.setTextureRect(IntRect(0, 0, w, h));
         x = X; y = Y;
         view.reset(FloatRect(x, y, 300, 300));
     }
@@ -29,38 +28,53 @@ public:
         case 1: dx = -speed; dy = 0; break;
         case 2: dx = speed; dy = 0; break;
         case 3: dx = 0; dy = -speed; break;
+        //case 4: dx = speed; dy = speed; break;
+        //case 5: dx = -speed; dy = speed; break;
+        //case 6: dx = speed; dy = -speed; break;
+        //case 7: dx = -speed; dy = -speed; break;
+        //case 8: dx = 0; dy = 0; break;
         }
         y += dy * time;
         x += dx * time;
         speed = 0;
-        /*bordersInteraction();*/
+        heroWithMapInteractions();
         sprite.setPosition(x, y);
         viewCentersOnHero(x, y);
-
     }
+
     View viewCentersOnHero(float x, float y) {
-        view.setCenter(x + 16, y + 24);
+        view.setCenter(x + w / 2, y + h / 2);
         return view;
     }
-   /* void bordersInteraction() {
-        float tempdx = dx;
-        float tempdy = dy;
-        for (int i = y / 32; i < (y + h) / 32; i++)
-            for (int j = x / 32; j < (x + w) / 32; j++) {
-                if (Map[i][j] == '0') {
-                    if (dy > 0) {
-                         y = i * 32 - h;
-                    }
-                    else if (dy < 0) {
-                        y = i * 32 + 32;
-                    }
-                    else if (dx > 0) {
-                        x = j * 32 - w;
-                    }
-                    else if (dx < 0) {
-                        x = j * 32 + 32;
-                    }
+    void heroWithMapInteractions() {
+        for (int i = x / rt; i < (x + w) / rt; i++)
+            for (int j = y / rt; j < (y + h) / rt; j++) {
+                if (MapLayout[i][j] == 'w' || MapLayout[i][j] == '0') {
+                    if (dx > 0 && dy == 0)
+                        x = i * rt - w;
+                    if (dx < 0 && dy == 0)
+                        x = i * rt + rt;
+                    if (dx == 0 && dy > 0)
+                        y= j * rt - h;
+                    if (dx == 0 && dy < 0)
+                        y = j * rt + rt;
+                    //if (dx > 0 && dy > 0) {
+                    //    x = i * rt - w;
+                    //    y = j * rt - h;
+                    //}
+                    //if (dx < 0 && dy > 0) {
+                    //    x = i * rt + rt;
+                    //    y = j * rt - h;
+                    //}
+                    //if (dx > 0 && dy < 0) {
+                    //    x = i * rt - w;
+                    //    y = j * rt + rt;
+                    //}
+                    //if (dx < 0 && dy < 0) {
+                    //    x = i * rt + rt;
+                    //    y = j * rt + rt;
+                    //}
                 }
             }
-    }*/
+    }
 };
--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,6 @@
 #include <SFML/Graphics.hpp>
-#include "map.h"
+//#include "map.h"
 #include "Hero.h"
-#include "view.h"
-#include "view.h"
 
 using namespace sf;
 using namespace std;
@@ -11,16 +9,15 @@ using namespace std;
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(500, 500), "The Quest");
-    /*view.reset(FloatRect(0, 0, 300, 300));*/
 
     Image mapImage;
-    mapImage.loadFromFile("images/map.png");
+    mapImage.loadFromFile("images/map2.png");
     Texture mapTexture;
     mapTexture.loadFromImage(mapImage);
     Sprite mapSprite;
     mapSprite.setTexture(mapTexture);
 
-    Hero hero("hero.png", 50, 50, 0, 0);
+    Hero hero(256, 256, 32, 48);
 
     float currentFrame = 0;
     Clock clock;
@@ -41,45 +38,69 @@ int main()
             hero.direction = 0; hero.speed = 0.1;
             currentFrame += 0.005 * time;
             if (currentFrame > 4) currentFrame -= 4;
-            hero.sprite.setTextureRect(IntRect(32 * int(currentFrame), 0, 32, 48));
+            hero.sprite.setTextureRect(IntRect(32 * int(currentFrame), 0, hero.w, hero.h));
         }
         if (Keyboard::isKeyPressed(Keyboard::A)) {
             hero.direction = 1; hero.speed = 0.1;
             currentFrame += 0.005 * time;
             if (currentFrame > 4) currentFrame -= 4;
-            hero.sprite.setTextureRect(IntRect(32 * int(currentFrame), 48, 32, 48));
+            hero.sprite.setTextureRect(IntRect(32 * int(currentFrame), 48, hero.w, hero.h));
         }
         if (Keyboard::isKeyPressed(Keyboard::D)) {
             hero.direction = 2; hero.speed = 0.1;
             currentFrame += 0.005 * time;
             if (currentFrame > 4) currentFrame -= 4;
-            hero.sprite.setTextureRect(IntRect(32 * int(currentFrame), 96, 32, 48));
+            hero.sprite.setTextureRect(IntRect(32 * int(currentFrame), 96, hero.w, hero.h));
         }
         if (Keyboard::isKeyPressed(Keyboard::W)) {
             hero.direction = 3; hero.speed = 0.1;
             currentFrame += 0.005 * time;
             if (currentFrame > 4) currentFrame -= 4;
-            hero.sprite.setTextureRect(IntRect(32 * int(currentFrame), 144, 32, 48));
+            hero.sprite.setTextureRect(IntRect(32 * int(currentFrame), 144, hero.w, hero.h));
         }
-        /*hero.bordersInteraction();*/
+        //if (Keyboard::isKeyPressed(Keyboard::S) && Keyboard::isKeyPressed(Keyboard::D)) {
+        //    hero.direction = 4; hero.speed = 0.1;
+        //    currentFrame += 0.0005 * time;
+        //    if (currentFrame > 4) currentFrame -= 4;
+        //    hero.sprite.setTextureRect(IntRect(32 * int(currentFrame), 0, hero.w, hero.h));
+        //}
+        //if (Keyboard::isKeyPressed(Keyboard::S) && Keyboard::isKeyPressed(Keyboard::A)) {
+        //    hero.direction = 5; hero.speed = 0.1;
+        //    currentFrame += 0.0005 * time;
+        //    if (currentFrame > 4) currentFrame -= 4;
+        //    hero.sprite.setTextureRect(IntRect(32 * int(currentFrame), 0, hero.w, hero.h));
+        //}
+        //if (Keyboard::isKeyPressed(Keyboard::W) && Keyboard::isKeyPressed(Keyboard::D)) {
+        //    hero.direction = 6; hero.speed = 0.1;
+        //    currentFrame += 0.0005 * time;
+        //    if (currentFrame > 4) currentFrame -= 4;
+        //    hero.sprite.setTextureRect(IntRect(32 * int(currentFrame), 144, hero.w, hero.h));
+        //}
+        //if (Keyboard::isKeyPressed(Keyboard::W) && Keyboard::isKeyPressed(Keyboard::A)) {
+        //    hero.direction = 7; hero.speed = 0.1;
+        //    currentFrame += 0.0005 * time;
+        //    if (currentFrame > 4) currentFrame -= 4;
+        //    hero.sprite.setTextureRect(IntRect(32 * int(currentFrame), 144, hero.w, hero.h));
+        //}
+        //if ((Keyboard::isKeyPressed(Keyboard::W) && Keyboard::isKeyPressed(Keyboard::S))) {
+        //    hero.direction = 8; hero.speed = 0.1;
+        //}
+
         hero.movement(time);
-        /*viewCentersOnHero(hero.x, hero.y);*/
         window.setView(hero.view);
         window.clear(); //сделать невыохдящую за пределы камеру
         for (int i = 0; i < mapHeight; i++)
             for (int j = 0; j < mapWidth; j++) {
-                if (Map[i][j] == ' ') {
-                    mapSprite.setTextureRect(IntRect(0, 0, 32, 32));
-                }
-                if (Map[i][j] == 's') {
-                    mapSprite.setTextureRect(IntRect(32, 0, 32, 32));
-                }
-                if (Map[i][j] == '0') {
-                    mapSprite.setTextureRect(IntRect(64, 0, 32, 32));
-                }
-                mapSprite.setPosition(i * 32, j * 32);
+                if (MapLayout[i][j] == '0') 
+                    mapSprite.setTextureRect(IntRect(0, 192, 64, 64));
+                if (MapLayout[i][j] == 'w')
+                    mapSprite.setTextureRect(IntRect(704, 64, 64, 64));
+                if (MapLayout[i][j] == ' ')
+                    mapSprite.setTextureRect(IntRect(64, 64, 64, 64));
+                mapSprite.setPosition(i * 64, j * 64);
                 window.draw(mapSprite);
             }
+
         window.draw(hero.sprite);
         window.display();
     }
